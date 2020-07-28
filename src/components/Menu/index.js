f94aa7f23c8aa120875f67ea8a57cb90ae442eaa
Alan Tavares <alan.tavares@superlogica.com>
2020-07-28 10:47:43 -0300

    adding pages

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css';
 import Button from '../Button';
+import { Link } from 'react-router-dom';
 
 function Menu(){
     return (
@@ -9,7 +10,7 @@ function Menu(){
             <a href='/'>
                 <img src={Logo} alt="Alanflix" className="Logo" />
             </a>
-            <Button href="/" className="ButtonLink">
+            <Button as={Link} to="/cadastro/video" className="ButtonLink">
                 Novo vídeo
             </Button>
         </nav>
